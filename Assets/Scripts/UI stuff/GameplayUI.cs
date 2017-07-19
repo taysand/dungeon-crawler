fdd15d1db04ad637e83e89643a8db24cf8ea94e2
taysand <tsandusky@mills.edu>
2017-07-19 10:44:02 -0700

    Show/hide gameplay ui when level up window is closed/open

---
+++ Assets/Scripts/UI stuff/GameplayUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayUI : MonoBehaviour
+{
+
+    static GameObject gm;
+    static bool activated;
+
+    // Use this for initialization
+    void Start()
+    {
+        gm = GetComponent<Canvas>().gameObject;
+        ShowGameplayUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static bool Activated()
+    {
+        return activated;
+    }
+
+    public static void ShowGameplayUI()
+    {
+        activated = true;
+        gm.SetActive(activated);
+    }
+
+    public static void HideGameplayUI()
+    {
+        activated = false;
+        gm.SetActive(activated);
+    }
+}
